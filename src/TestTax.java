983969e07e7d7b57971d05c6d7202dd37bbf0ad8
Jarod <konstantin.zalevsky@gmail.com>
2019-05-05 01:25:50 +0300

    add two new classes

--- src/TestTax.java
+++ src/TestTax.java
@@ -1,11 +1,14 @@
 public class TestTax {
     public static void main(String[] args) {
-        Tax t = new Tax();
+//        Tax t = new Tax();// закоментили строчку, т.к. тестим класс NJTax, что бы увидеть расчет.
+        NJTax t = new NJTax();
         t.grossIncome = 50000;
         t.numberOfDependens = 2;
         t.state = "NJ";
 
         double yourTax = t.calcTax();
-        System.out.println("You tax is " + yourTax);// Напечать вывело 4 тыс, т.к. 50 тыс => дохода и пошло на 8%
+        double njt = t.adjustForStudents(yourTax);
+
+        System.out.println("You tax is " + njt);// Напечать вывело 4 тыс, т.к. 50 тыс => дохода и пошло на 8%
     }
 }
